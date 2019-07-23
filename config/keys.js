9977d5977e958f041fd5eb7f71dcbf35e2fe5cf5
Ashis Jena <ashis.jena96@gmail.com>
2019-07-24 01:15:16 +0530

    add authentication and authorization in backend

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI : "mongodb+srv://ashis:ashis@cluster0-hhmz3.mongodb.net/test?retryWrites=true&w=majority"
-}
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
+const config = require('config');
 
 const items = require('./routes/api/items');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to Mongo
 mongoose
@@ -24,7 +26,8 @@ mongoose
 
 // Use Routes
 app.use('/api/items',items);
-
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
